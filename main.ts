7bb2e25988edcd87e61c3b36aebc236eb2e5818b
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-12-19 00:29:59 -0500

    fixed formatting

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Lucas Nguyen
+ * Created on: Dec 2024
+ * This program drives, then reverses when too close to an object
 */
 
-basic.showString('Hello, World!')
+// defining variables and showing happy face
+basic.showIcon(IconNames.Happy)
+let distanceOfObject: number
+
+// while true statement
+while (true) {
+    distanceOfObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // if distance is greater than 10
+    if (distanceOfObject > 10) {
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(10, 48)
+    } else {
+        basic.showIcon(IconNames.No)
+        robotbit.StpCarMove(0, 0)
+        basic.pause(500)
+
+        // backup
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(1000)
+
+        // turn 90 degree
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+        basic.pause(500)
+        robotbit.StpCarMove(10, 48)
+    }
+}
